857b1dd33f74273107a55e09612f1cc17064c402
Andrey Kamakin <a.kamakin@icloud.com>
2020-07-11 18:57:13 +0300

    added create/delete methods

--- users_service/main.go
+++ users_service/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
 
 	pb "github.com/lieroz/dips-coursework-twitter/users_service/protos"
 )
 
 var (
-	ctx = context.Background()
 	rdb *redis.ClusterClient
 )
 
@@ -25,7 +27,45 @@ type UsersServerImpl struct {
 }
 
 func (s *UsersServerImpl) CreateUser(ctx context.Context, in *pb.CreateRequest) (*pb.EmptyReply, error) {
+	if in.GetUsername() == "" {
+		return &pb.EmptyReply{}, fmt.Errorf("username field can't be empty")
+	}
+
+	user := pb.User{
+		Username:              in.Username,
+		Firstname:             in.Firstname,
+		Lastname:              in.Lastname,
+		Description:           in.Description,
+		RegistrationTimestamp: time.Now().Unix(),
+	}
+
+	proto, err := proto.Marshal(&user)
+	if err != nil {
+		return &pb.EmptyReply{}, err
+	}
+
+	notExist, err := rdb.SetNX(ctx, in.Username, proto, 0).Result()
+	if err != nil {
+		return &pb.EmptyReply{}, err
+	}
+	if !notExist {
+		return &pb.EmptyReply{}, fmt.Errorf("user with username: '%s' already exists", in.Username)
+	}
 	return &pb.EmptyReply{}, nil
+}
+
+func (s *UsersServerImpl) DeleteUser(ctx context.Context, in *pb.DeleteRequest) (*pb.EmptyReply, error) {
+	if in.GetUsername() == "" {
+		return &pb.EmptyReply{}, fmt.Errorf("username field can't be empty")
+	}
+	if err := rdb.Del(ctx, in.Username).Err(); err != nil {
+		return &pb.EmptyReply{}, err
+	}
+	return &pb.EmptyReply{}, nil
+}
+
+func (s *UsersServerImpl) GetUserInfoSummary(ctx context.Context, in *pb.GetSummaryRequest) (*pb.GetSummaryReply, error) {
+	return &pb.GetSummaryReply{}, nil
 }
 
 func main() {
